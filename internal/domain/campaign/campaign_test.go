2675d8e5fdcea90d75785a0dbf7ff4bc4a2dc3a0
Mateus Marques <mateus.mqes@gmail.com>
2023-06-17 12:10:45 -0300

    feat: add xid package and add new tests for campaign

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,11 +2,12 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
 	name := "Campaign X"
 	content := "Content X"
@@ -14,8 +15,52 @@ func TestNewCampaign(t *testing.T) {
 
 	campaign := NewCampaign(name, content, contacts)
 
-	assert.Equal("1", campaign.ID)
 	assert.Equal(name, campaign.Name)
 	assert.Equal(content, campaign.Content)
 	assert.Equal(len(contacts), len(campaign.Contacts))
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Content X"
+	contacts := []string{"emai1@email.com", "email2@email.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_NameIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Content X"
+	contacts := []string{"emai1@email.com", "email2@email.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.Name)
+}
+
+func Test_NewCampaign_ContentIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Content X"
+	contacts := []string{"emai1@email.com", "email2@email.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.Content)
+}
+
+func Test_NewCampaign_CreatedOnIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Content X"
+	contacts := []string{"emai1@email.com", "email2@email.com"}
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.Greater(campaign.CreatedOn, now)
 }
