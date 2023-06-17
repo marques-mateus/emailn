41c849d9a113bd247d069f1c9503f17d9803b2f5
Mateus Marques <mateus.mqes@gmail.com>
2023-06-17 11:58:48 -0300

    test: add testfy package and refactor campaign tests

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,28 +2,20 @@ package campaign
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCampaign(t *testing.T) {
+	assert := assert.New(t)
 	name := "Campaign X"
 	content := "Content X"
 	contacts := []string{"emai1@email.com", "email2@email.com"}
 
 	campaign := NewCampaign(name, content, contacts)
 
-	if campaign.ID != "1" {
-		t.Errorf("campaign.ID = %v; want %v", campaign.ID, "1")
-	}
-
-	if campaign.Name != name {
-		t.Errorf("campaign.Name = %v; want %v", campaign.Name, name)
-	}
-
-	if campaign.Content != content {
-		t.Errorf("campaign.Content = %v; want %v", campaign.Content, content)
-	}
-
-	if len(campaign.Contacts) != len(contacts) {
-		t.Errorf("campaign.Contacts = %v; want %v", len(campaign.Contacts), len(contacts))
-	}
+	assert.Equal("1", campaign.ID)
+	assert.Equal(name, campaign.Name)
+	assert.Equal(content, campaign.Content)
+	assert.Equal(len(contacts), len(campaign.Contacts))
 }
